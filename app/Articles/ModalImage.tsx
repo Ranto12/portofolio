301e5eb5ddb4da6479c811b10e5af5c0a927dacc
ranto <rantoirwanda08@gmail.com>
2024-03-05 15:27:17 +0700

    oke oke

--- app/Articles/ModalImage.tsx
+++ app/Articles/ModalImage.tsx
@@ -1,35 +1,39 @@
-"use client"
-import { useDisclosure } from '@mantine/hooks';
-import NextImage from 'next/image';
-import { Modal, Button, Image as Nice } from '@mantine/core';
-import { ReactAltera,Aws } from './asset';
-import Image from 'next/image';
+"use client";
+import { useDisclosure } from "@mantine/hooks";
+import NextImage from "next/image";
+import { Modal, Button, Image as Nice } from "@mantine/core";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 interface Props {
-        image: any
+  image: any;
 }
 
-function Modalemage({image}: Props) {
+function Modalemage({ image }: Props) {
   const [opened, { open, close }] = useDisclosure(false);
 
   return (
     <>
-      <Modal bg="blue" size="xl" opened={opened} onClose={close} withCloseButton={false} centered>
-      <Nice
-        alt='nice'
-        radius="md"
-        src={image}
-        component={NextImage}
-        />
+      <Modal
+        bg="blue"
+        size="xl"
+        opened={opened}
+        onClose={close}
+        withCloseButton={false}
+        centered
+      >
+        <motion.div>
+          <Nice alt="nice" radius="md" src={image} component={NextImage} />
+        </motion.div>
       </Modal>
       <Image
-        alt='nice'
+        alt="nice"
         width={400}
         height={400}
         src={image}
         onClick={open}
-        className='cursor-pointer rounded-md'
-        />
+        className="cursor-pointer rounded-md"
+      />
     </>
   );
 }
